aea0b61660537384e142882db392aa41e4b45de0
逸雷 沈 <tzsylsyl@163.com>
2020-05-14 00:33:43 +0800

    basic structure for base class

---
+++ CADproj/CADElement.cpp
@@ -0,0 +1,16 @@
+#define __CADELEMENTS_GLOBALS
+
+#include <CADElement.h>
+
+using namespace std;
+
+
+
+CADElement::CADElement()
+{
+	id=0;
+}
+
+CADElement::~CADElement()
+{
+}
--- CADproj/CADElement.h
+++ CADproj/CADElement.h
@@ -1,1 +1,32 @@
-#pragma once
+#ifndef __CADELEMENTS_H
+#define __CADELEMENTS_H
+
+#include <string>
+
+using namespace std;
+
+
+
+class CADElement
+{
+public:
+	CADElement();
+	//virtual CADElement(const CADElement& insta)=0;
+	//virtual CADElement& operator=(const CADElement& insta)=0;
+	~CADElement();
+
+	virtual void init()=0;
+	virtual void draw()=0;
+	virtual void move(float dx,float dy)=0;
+	virtual void modify()=0;
+	virtual void save(string path)=0;
+
+
+protected:
+	int id;
+
+};
+
+
+
+#endif
