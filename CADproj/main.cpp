3f77462c647c86c23c1591a00fc1dc16c1e037eb
逸雷 沈 <tzsylsyl@163.com>
2020-05-13 15:35:29 +0800

    experiment,basic graphics,mouse detect

--- CADproj/main.cpp
+++ CADproj/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <graphics.h>
+#include <conio.h>
+#include <Windows.h>
 
 using namespace::std;
 
@@ -14,8 +16,48 @@ using namespace::std;
   */
 int main()
 {
-	initgraph(640,480);
-	cin.get();
+	initgraph(1280,720);
 
+	//BeginBatchDraw();
+	//while(1)
+	//{
+	//    for(int i=0;i<64;i++)
+	//    {
+	//        for(int j=0;j<36;j++)
+	//        {
+	//            circle(20*i+10,20*j+10,10);
+	//            FlushBatchDraw();
+	//            Sleep(10);
+	//            cleardevice();
+	//        }
+	//    }
+	//}
+	//EndBatchDraw();
+
+	setaspectratio(1,1);
+	float px=0;
+	float py=0;
+	//getaspectratio(px,py);		//bug???????
+	TCHAR s[127];
+	_stprintf_s(s,127,_T("%f,%f"),px,py);
+	outtextxy(0,0,s);
+
+	MOUSEMSG currentMouse=GetMouseMsg();
+	MOUSEMSG previousMouse=currentMouse;
+	int i=0;
+	while(1)
+	{
+		currentMouse=GetMouseMsg();
+		if(previousMouse.x==currentMouse.x&&previousMouse.y==currentMouse.y)continue;
+		i++;
+		_stprintf_s(s,127,_T("%d,%d,%d"),currentMouse.x,currentMouse.y,currentMouse.wheel);
+		outtextxy((i*20)/720*120,(i*20)%720,s);
+
+		previousMouse=currentMouse;
+	}
+
+
+	cin.get();
+	closegraph();
 	return 0;
 }
