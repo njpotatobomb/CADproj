cec261c37d37c90048f8c90b602bedeb6ff04675
逸雷 沈 <tzsylsyl@163.com>
2020-05-13 14:18:10 +0800

    created project

---
+++ CADproj/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <graphics.h>
+
+using namespace::std;
+
+
+
+
+/**
+  * @brief      main
+  * @param   none
+  * @retval     none
+  * @author
+  */
+int main()
+{
+	initgraph(640,480);
+	cin.get();
+
+	return 0;
+}
